e66cf5e6d8e917f90035b7d8c79e0eddf49b28d0
عاصم محمود <asim16111980@gmail.com>
2025-03-16 21:24:13 +0200

    Create Home Page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,33 +1,25 @@
-import Button from '../components/Button';
 import Carousel from '../components/Carousel';
-import CategoryPhone from '../components/CategoryPhone';
 import FlashSaleTimer from '../components/FlashSaleTimer';
 import NavigationButtons from '../components/NavigationButtons';
 import SectionHeader from '../components/SectionHeader';
 import { MenusContext } from '../context/MenusProvider';
 import { useContext } from 'react';
-import FlatCard from '../components/FlatCard';
-import DiscCard from '../components/DiscCard';
-import BlackCard from '../components/BlackCard';
-import ServiceCard from '../components/ServiceCard';
+
 
 const Home = () => {
     const { state, dispatch } = useContext(MenusContext);
     return (
         <div className='w-full h-screen' onClick={() => dispatch({ currentMenu: null })}>
             <Carousel />
-            <div className='flex flex-col gap-8'>
-                <SectionHeader title={"Today’s"} subTitle={"Flash Sales"} />
-                <FlashSaleTimer />
-                <NavigationButtons />
-                <FlatCard img={"/public/images/cart/g92-2-500x500 1.png"} alt={""} title={"HAVIT HV-G92 Gamepad"} netPrice={120} rating={3.5} rateCount={88} productColorOptions={[{ id: "red", color: "red", state: false }, { id: "blue", color: "blue", state: false }]} />
-                <DiscCard img={"/public/images/cart/g92-2-500x500 1.png"} alt={""} discount={40} title={"HAVIT HV-G92 Gamepad"} netPrice={120} totalPrice={160} rating={3.5} rateCount={88} productColorOptions={[{ id: "red", color: "red", state: false }, { id: "blue", color: "blue", state: false }]} />
-                <Button style={"px-12 py-4"} value={"View All Products"} />
-                <Button style={"px-4 py-2"} value={"View All Products"} />
-                <Button filled={false} style={"px-12 py-4"} value={"View All Products"} />
-                <CategoryPhone img={"/public/images/phone/Category-CellPhone.svg"} title={"Phones"} style={"w-20 h-24"} />
-                <BlackCard cardStyle={""} img={"/public/images/cart/g92-2-500x500 1.png"} alt={""} />
-                <ServiceCard img="/public/images/service/icon-delivery.svg" alt="" title="FREE AND FAST DELIVERY" subTitle="Free delivery for all orders over $140" />
+            <div className='flex flex-col gap-8 px-2 py-4'>
+                <section>
+                    <header className='flex flex-col items-center gap-3'>
+                        <SectionHeader title="Today’s" subTitle="Flash Sales" />
+                        <FlashSaleTimer />
+                        <NavigationButtons />
+                    </header>
+
+               </section>
             </div>
         </div>
     )
--- src/components/SectionHeader.jsx
+++ src/components/SectionHeader.jsx
@@ -1,7 +1,7 @@
 const SectionHeader = ({ title, subTitle }) => {
   return (
-    <div className="">
-      <div className="w-24 px-4 py-1 bg-[#DB4444] rounded mb-3">
+    <div className="flex flex-col items-center gap-2">
+      <div className="w-24 px-4 py-1 bg-[#DB4444] rounded">
         <span className="hidden bg-[#DB4444]"></span>
         <h2 className='font-["poppins"] text-lg text-white'>{title}</h2>
       </div>
