06a6561bd2f5f462b97d30de75e98459e8803073
asim16111980 <abomido2892016@gmail.com>
2025-03-30 01:43:15 +0200

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"https://asim16111980.github.io/Week_Four_Task_One/"
+  base:"https://github.com/asim16111980/Week_Four_Task_One.git"
 })
