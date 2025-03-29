197b491ae3811d0e466a7f7829af5ee974103527
asim16111980 <abomido2892016@gmail.com>
2025-03-30 01:51:53 +0200

    Deploy to GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-// import { StrictMode } from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HashRouter as Router } from "react-router-dom";
-import { SearchBoxProvider } from "/context/SearchBoxProvider";
-import { MenusProvider } from "/context/MenusProvider";
-import "/index.css";
-import App from "/App.jsx";
+import { BrowserRouter as Router } from "react-router-dom";
+import { SearchBoxProvider } from "./context/SearchBoxProvider";
+import { MenusProvider } from "./context/MenusProvider";
+import "./index.css";
+import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
-  // <StrictMode>
+  <StrictMode>
     <Router>
       <SearchBoxProvider>
         <MenusProvider>
@@ -15,5 +15,5 @@ createRoot(document.getElementById("root")).render(
         </MenusProvider>
       </SearchBoxProvider>
     </Router>
-  // </StrictMode>
+  </StrictMode>
 );
